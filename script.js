5ba6fd8abec3d7d9bea7cbb9e3b26ca24a646cd6
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-09-05 07:09:51 +0900

    Fix the input function. There is still an error with the value attributes of the inputs

--- script.js
+++ script.js
@@ -2,39 +2,54 @@ const background = document.querySelector("div")
 const inputBackgroundSize = document.querySelector("input.backgroundSize")
 const inputSideSquares = document.querySelector("input.sideSquares")
 
-inputBackgroundSize.addEventListener("keypress", e => {
-    if(e.key === "Enter"){
-        let backgroundSize = inputBackgroundSize.value
-        
-        
+
+setBackground()
+fillBackground()
+
+inputSideSquares.addEventListener("keypress", function(e){
+    if(e.key === "Enter" && inputSideSquares.value < 100){
+        fillBackground()
     }
 })
+inputBackgroundSize.addEventListener("keypress", function(e){
+    if(e.key === "Enter" && inputBackgroundSize.value < 800){
+        setBackground()
+    }
+})    
+function getSideSquares(){
+    return parseInt(inputSideSquares.value)  
+}
 
-let backgroundSize = 600
-
-let sideSquares = 64
-let maxSquares = sideSquares*sideSquares
-let subtractBorder = sideSquares*2
-let gridItemHeight = ((backgroundSize-subtractBorder)/sideSquares)
-let gridItemWidth = ((backgroundSize-subtractBorder)/sideSquares)
-
-
-background.style.height = `${backgroundSize}px`
-background.style.width = `${backgroundSize}px`
+function setBackground(){
+    let backgroundSize = parseInt(inputBackgroundSize.value)
+    background.style.height = `${backgroundSize}px`
+    background.style.width = `${backgroundSize}px`
+    return backgroundSize
+}
 
 function fillBackground(){
+    let backgroundSize = setBackground()
+    let sideSquares = getSideSquares()
+    let maxSquares = sideSquares*sideSquares
+    let subtractBorder = sideSquares*2
+    let gridItemHeight = ((backgroundSize-subtractBorder)/sideSquares)
+    let gridItemWidth = ((backgroundSize-subtractBorder)/sideSquares)
+    console.log(sideSquares)
+    console.log(maxSquares)
+    console.log(gridItemHeight)
+
     for(let i=0; i<maxSquares; i++){
         const gridItem = document.createElement("div")
         background.appendChild(gridItem)
         gridItem.setAttribute("class","gridItem")
         gridItem.style.height = `${gridItemHeight}px`
         gridItem.style.width = `${gridItemWidth}px`
-        gridItem.addEventListener("mouseover", e => {
+        gridItem.addEventListener("mousedown", e => {
             gridItem.setAttribute("class", "marked")
         })
     }
 }
 
-fillBackground();
+
 
 
